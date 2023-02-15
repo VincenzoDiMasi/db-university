2f959be96d7a22c5282d092f4e5cabfe080b8ca1
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-15 15:16:59 +0100

    Add 2 query w join

--- JOIN queries.sql
+++ JOIN queries.sql
@@ -8,6 +8,11 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
+SELECT `degrees`.`name` AS 'Course', `departments`.`name` AS 'Department'
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
