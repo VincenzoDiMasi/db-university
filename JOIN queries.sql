784c55d11421f7ed4cd6706b552f8807d017c12f
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-15 16:09:10 +0100

    Add 6 query w join

--- JOIN queries.sql
+++ JOIN queries.sql
@@ -50,6 +50,16 @@ ON `teachers`.`id` = `course_teacher`.`teacher_id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
-
+SELECT DISTINCT `departments`.`name` AS 'Department', `teachers`.`name` AS 'Name', `teachers`.`surname` AS 'Surname'
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica';
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
